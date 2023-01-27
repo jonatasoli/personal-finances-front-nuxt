65c778b0023e0d4242f2d119db61d59b062549a0
Jonatas Oliveira <contact@jonatasoliveira.dev>
2023-01-26 22:49:09 -0300

    chore: add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   buildModules: [
-    'nuxt-vite'
-  ]
+    'nuxt-vite',
+    '@nuxt/postcss8',
+  ],
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
+  css: [
+    '@/assets/css/main.css',
+  ],
 })
